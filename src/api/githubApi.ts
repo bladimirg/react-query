f3686c5f2ea0dab11fda0a74270a416c0695f5a2
Bladimir Gonzales <bladimirg@gmail.com>
2023-03-04 15:16:51 +0100

    Update githubApi.ts

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -3,7 +3,5 @@ import axios from "axios";
 
 export const githubApi= axios.create({
     baseURL:'https://api.github.com/repos/facebook/react',
-    headers:{
-        authorization: 'bearer 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'
-    }
+    headers:{}
 });
